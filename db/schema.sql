43d8591948e1006066568d65d75e9b671c7d6104
Sushi C <chantal.otum@gmail.com>
2019-11-05 10:42:27 -0500

    table change

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE Products (
+CREATE TABLE burgers (
   id INTEGER(10) AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(50) NOT NULL,
   devoured BOOLEAN NOT NULL,
