3fe9ff0b82cedd0f443e01566edb59d112ec1878
Sushi C <chantal.otum@gmail.com>
2019-11-05 10:37:18 -0500

    seed values

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lamb Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Truffle Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Burger", TRUE);
