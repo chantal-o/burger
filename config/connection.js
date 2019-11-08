13e1066bbb8151bb077827e31dadcba271d79dc0
Sushi C <chantal.otum@gmail.com>
2019-11-07 22:15:57 -0500

    used npm documentation for connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,28 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+// initialize MySQL connection
+var mysql = require("mysql");
+var connection;
+
+// connect to db
+
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
     database: "burgers_db"
- });
- 
- connection.connect(function(err) {
-    if (err) throw err;
+    });
 
 
+// Make connection to database following msql npm documentation: https://www.npmjs.com/package/mysql
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
   });
-// Export the connection.
-  module.exports = connection;
+
+  
+// export connection 
+module.exports = connection;
